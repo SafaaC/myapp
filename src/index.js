898909024ec5ea3e5570ee8511ea94f32e1a999a
SafaaC <safaaacherak@gmail.com>
2023-08-22 13:09:28 +0530

    onClick event

--- src/index.js
+++ src/index.js
@@ -12,19 +12,23 @@ const styleObj1={
   backgroundColor:"black",
   padding:"5px",
 }
+const onClick=()=> console.log("clicked");
 const arr=["Item1" , "Item2" ,"Item3"];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div id="myId" className='class' style={styleObj1}>
-          <p>Since class is a keyword use className as an attribute in html tag.for the same reason use htmlFor
+          <p onClick={onClick}>Since class is a keyword use className as an attribute in html tag.for the same reason use htmlFor
           instead of for attribute.For giving inline style to tag , make an object and define styles as key: value pair
           where the key should not have - or space use Camel cases(background-color=backgroundColor),and 
           pass the object within curely braces</p>
-          <p>we can pass only a sigle statement in curely braces</p>
+          <p onClick={function(){
+            console.log("clicked");
+            console.log("para clicked");
+          }}>we can pass only a sigle statement in curely braces</p>
           {
             arr.map((item,index)=><label key={index}> {item} </label>)
           }
-          
+         
     </div>
     // JSX statement is converted to js as
     // React.createElement("div",
