e96a72e2bd2f0d1d9a75a2806f61962aaaf405e8
SafaaC <safaaacherak@gmail.com>
2023-08-23 14:11:20 +0530

    children props

--- src/body/listItem.js
+++ src/body/listItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Tools from './tools';
 
 //function ListItem({item1,item2}){  //parameter de structuring
 function ListItem(props){   // can give any name as param
@@ -6,8 +7,10 @@ function ListItem(props){   // can give any name as param
    
     return (
         <div>
-        <p onClick={onAction(item1) }>{item1}</p>
+        <Tools>
+        <p onClick={()=>onAction(item1) }>{item1}</p>
         <p onClick={()=>console.log(item2)}>{item2}</p>
+        </Tools>
         </div>
     )
 }
---
+++ src/body/tools.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Tools extends React.Component{
+    render(){
+        const{children}=this.props;
+        return children;
+    }
+} 
+
+export default Tools;
