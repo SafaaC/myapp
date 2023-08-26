65087dbf74fb6e0aeb77ad959b8649dc7f2b74a3
SafaaC <safaaacherak@gmail.com>
2023-08-26 12:55:48 +0530

    Changed Folder name&SetUpfor Product List

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import BodyCard from './body/BodyCard.js';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
+// import BodyCard from './Demo/BodyCard.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BodyCard/>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BodyCard/>
+// );
